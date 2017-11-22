0c646e3e83f190064a2345f1cd2846c915e4942e
Platemiy <wobbuffet@bk.ru>
2017-11-22 13:52:57 +0300

    Initial commit

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textUpload = new System.Windows.Forms.TextBox();
+            this.textCount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(12, 28);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 0;
+            this.buttonRead.Text = "Читать";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(109, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Время загрузки и сохранения:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textUpload
+            // 
+            this.textUpload.BackColor = System.Drawing.SystemColors.Window;
+            this.textUpload.Location = new System.Drawing.Point(112, 31);
+            this.textUpload.Name = "textUpload";
+            this.textUpload.ReadOnly = true;
+            this.textUpload.Size = new System.Drawing.Size(127, 20);
+            this.textUpload.TabIndex = 3;
+            // 
+            // textCount
+            // 
+            this.textCount.BackColor = System.Drawing.SystemColors.Window;
+            this.textCount.Location = new System.Drawing.Point(112, 74);
+            this.textCount.Name = "textCount";
+            this.textCount.ReadOnly = true;
+            this.textCount.Size = new System.Drawing.Size(127, 20);
+            this.textCount.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол-во слов:";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(112, 121);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(127, 20);
+            this.textSearch.TabIndex = 6;
+            this.textSearch.TextChanged += new System.EventHandler(this.textSearch_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Поиск ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Search
+            // 
+            this.Search.FormattingEnabled = true;
+            this.Search.Location = new System.Drawing.Point(13, 154);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(120, 95);
+            this.Search.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(139, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Время поиска:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textCount);
+            this.Controls.Add(this.textUpload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textUpload;
+        private System.Windows.Forms.TextBox textCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Search;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
